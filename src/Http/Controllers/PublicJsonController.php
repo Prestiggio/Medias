26c479bc4937d7cf6e2d0f2ddbb58dcba1d4e53c
folojona@gmail.com <topgasy@tsikawa.com>
2017-05-05 16:48:56 +0200

    support upload fb

--- src/Http/Controllers/PublicJsonController.php
+++ src/Http/Controllers/PublicJsonController.php
@@ -10,6 +10,8 @@ use Ry\Medias\Models\Resizing;
 use Illuminate\Support\Facades\File;
 use PHPImageWorkshop\ImageWorkshop;
 use Gregwar\Image\Image;
+use Facebook\Facebook;
+use Facebook\GraphNodes\GraphObject;
 
 class PublicJsonController extends Controller
 {
@@ -17,11 +19,7 @@ class PublicJsonController extends Controller
 		return Auth::user();
 	}
 	
-	public function getTest() {
-		return Auth::user();
-	}
-	
-	public function postSave(Request $request) {
+	public function postSave(Request $request) {		
 		$file = $request->file("file");
 		
 		/*
@@ -34,8 +32,24 @@ class PublicJsonController extends Controller
 		 -F "description[gugus]=dffgg"
 		*/
 	
-		$path = time() . "-" . $file->getClientOriginalName();
-		$file->move(public_path("uploads"), $path);
+		try {
+			$fb = new Facebook([
+					'app_id' => "691462271025098",
+					"app_secret" => "635f60e1510231ea5bb5cae9a3f60b47",
+					"default_graph_version" => "v2.8"
+			]);
+			$fb->setDefaultAccessToken("EAAJ04ZAsKP8oBAIvKr5TBXvMUMcsgOb9gqRAkZAQQ2QQWNUJ3g9ZA9ZAGFGEIs12gZBzCas46d7dgcxiuZBkw796RnwpC82hLMpcMBtGIgksp7zcCSswW3RN1NEdbFSVxBasuPVEOuP37Fmw4MiNyzBFeV4X0N9hBorI5gpQXwAtwtn9ZCPO29rFiyovbOZCz9YZD");
+			$fbrequest = $fb->request('POST', '/541064206085755/photos', [
+					"message" => "Uploaded photo",
+					"source" => $fb->fileToUpload($file->getPathname())
+			]);
+			$response = $fb->getClient()->sendRequest($fbrequest)->getGraphNode();
+			$path = "https://graph.facebook.com/" . $response->getField("id", 666834146842093) . "/picture";
+		}
+		catch(\Exception $e) {
+			$path = time() . "-" . $file->getClientOriginalName();
+			$file->move(public_path("uploads"), $path);
+		}
 		
 		Media::unguard();
 		
