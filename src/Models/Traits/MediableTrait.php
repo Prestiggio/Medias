9856e493ee99c5aa3a880d131c1f4086bf4675e6
Top Mora <manager@topmora.com>
2017-06-30 20:45:46 +0200

    Kipa -  jour support image par defaut

--- src/Models/Traits/MediableTrait.php
+++ src/Models/Traits/MediableTrait.php
@@ -6,4 +6,19 @@ trait MediableTrait
 	public function medias() {
 		return $this->morphMany("Ry\Medias\Models\Media", "mediable");
 	}
+	
+	public function getThumbAttribute() {
+		if($this->thumbnail)
+			return $this->thumbnail;
+		
+		foreach($this->medias as $media) {
+			return $media->url;
+		}
+		 
+		return $this->default_thumb;
+	}
+	
+	public function thumbnail() {
+		return $this->belongsTo("Ry\Medias\Models\Media", $this->thumbnail_field);
+	}
 }
